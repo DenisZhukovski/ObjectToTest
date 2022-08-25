cfa776991ee5341740521c6dae5dba28c0063df9
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-08-25 19:04:07 +0300

    implement generic type recognition, returning properly configured generic constructor name

---
+++ tests/ObjectToTest.UnitTests/Models/With2GenericArguments.cs
@@ -0,0 +1,15 @@
+﻿namespace ObjectToTest.UnitTests.Data
+{
+    public class With2GenericArguments<T, T2>
+    {
+        public T Create()
+        {
+            return default(T);
+        }
+
+        public T2 Create2()
+        {
+            return default(T2);
+        }
+    }
+}
