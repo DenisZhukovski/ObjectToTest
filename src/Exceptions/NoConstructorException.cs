66424808a5e3b0b0fec2c469ebf28887f1ea3c04
Maksym Shustov <Maksym_Shustov@epam.com>
2022-08-27 18:57:00 +0200

    Added missing artibute and constructor to custom exception type

--- src/Exceptions/NoConstructorException.cs
+++ src/Exceptions/NoConstructorException.cs
@@ -5,12 +5,18 @@ using System.Text;
 
 namespace ObjectToTest.Exceptions
 {
+    [Serializable]
     public class NoConstructorException : Exception
     {
         private const string _messageFormat = "Can not find a constructor for {0} object, not valid constructor available";
 
         public NoConstructorException(Type type)
             : base(string.Format(_messageFormat, type.Name))
+        {
+        }
+
+        protected NoConstructorException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
