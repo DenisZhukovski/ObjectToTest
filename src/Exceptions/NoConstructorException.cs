e36ebd766916af99e3b09fcdebded23b84f138f5
Maksym Shustov <Maksym_Shustov@epam.com>
2022-08-27 18:48:55 +0200

    Added new exception type. Updated ToString for ObjectAsConstructor

---
+++ src/Exceptions/NoConstructorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ObjectToTest.Exceptions
+{
+    public class NoConstructorException : Exception
+    {
+        private const string _messageFormat = "Can not find a constructor for {0} object, not valid constructor available";
+
+        public NoConstructorException(Type type)
+            : base(string.Format(_messageFormat, type.Name))
+        {
+        }
+    }
+}
