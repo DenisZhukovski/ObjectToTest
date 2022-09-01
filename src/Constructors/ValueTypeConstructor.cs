c399d46578f370dfb1803a16518ffb64d0680da5
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-09-01 18:42:08 +0300

    Add Value Type constructor

---
+++ src/Constructors/ValueTypeConstructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ObjectToTest.ConstructorParameters;
+using System.Collections.Generic;
+
+namespace ObjectToTest.Constructors
+{
+    internal class ValueTypeConstructor : IConstructor
+    {
+        private readonly object _object;
+
+        public ValueTypeConstructor(object @object)
+        {
+            _object = @object;
+        }
+
+        public bool IsValid => true;
+
+        public IList<IArgument> Argumetns => new List<IArgument>();
+
+        public override string ToString()
+        {
+            return _object.ToStringForInialization();
+        }
+    }
+}
+
