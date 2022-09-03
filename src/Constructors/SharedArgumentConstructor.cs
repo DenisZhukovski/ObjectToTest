eada8ef511aa98498b9f503509bdaf3936c5f238
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-09-03 21:12:45 +0300

    Add shared argument class

---
+++ src/Constructors/SharedArgumentConstructor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using ObjectToTest.Arguments;
+
+namespace ObjectToTest.Constructors
+{
+    public class SharedArgumentConstructor : IConstructor
+    {
+        private readonly IArgument _argument;
+        private string _initVariable = string.Empty;
+
+        public SharedArgumentConstructor(IArgument argument)
+        {
+            _argument = argument;
+        }
+
+        bool IConstructor.IsValid => _argument.Constructor.IsValid;
+
+        IList<IArgument> IConstructor.Argumetns => _argument.Constructor.Argumetns;
+
+        public override bool Equals(object? obj)
+        {
+            return _argument.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _argument.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(_initVariable))
+            {
+                _initVariable = $"var {_argument.Name} = {_argument.Constructor}";
+            }
+            return _argument.Name;
+        }
+    }
+}
+
