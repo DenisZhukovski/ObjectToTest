cfdd3568528ca19d04461d31d714e42949da7373
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-09-06 09:06:41 +0300

    improve the unit test @todo #12:60m/DEV Improve the test: Ideally the test should compare the result that returned by Argument method  with some manually created argument that wraps the original object.

--- tests/ObjectToTest.UnitTests/ObjectSharedArgumentsTests.cs
+++ tests/ObjectToTest.UnitTests/ObjectSharedArgumentsTests.cs
@@ -10,14 +10,6 @@ namespace ObjectToTest.UnitTests
     public class ObjectSharedArgumentsTests
     {
         [Fact]
-        /**
-           * @todo #12:60m/DEV Improve the test: Ideally the test should compare the result that returned by Argument method
-           * with some manually created argument that wraps the original object.
-           * Assert.Equal(
-           *    new Argument(user),
-           *    new ObjectSharedArguments(withUser).Argument(user)
-           * );
-           */
         public void SharedNotNullArgument()
         {
             var user = new User("user name");
@@ -28,7 +20,8 @@ namespace ObjectToTest.UnitTests
                     User = user
                 }
             );
-            Assert.NotNull(
+            Assert.Equal(
+                new Argument("user", user.ValidConstructor(new MockArguments())),
                 new ObjectSharedArguments(withUser).Argument(user)
             );
         }
