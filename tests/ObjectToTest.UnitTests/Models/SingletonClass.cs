54d178636b7a952527d7d3dfe024db2788827fb6
Denis Zhukovski <46710314+DenisZhukovski@users.noreply.github.com>
2022-09-07 11:55:40 +0300

    Singleton object (#28)
    * fix the unit test TheSameObjectDetection
    
    * remove the puzzle
    
    * add puzzle for singletons
    
    Co-authored-by: dzhukovski <69575528+dzhukovski@users.noreply.github.com>

---
+++ tests/ObjectToTest.UnitTests/Models/SingletonClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ObjectToTest.UnitTests.Models
+{
+    public class SingletonClass
+    {
+        private static SingletonClass _instance;
+
+        private SingletonClass()
+        {
+        }
+
+        public static SingletonClass Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingletonClass();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
---
+++ tests/ObjectToTest.UnitTests/Models/WithSingletonArgument.cs
@@ -0,0 +1,14 @@
+﻿using ObjectToTest.UnitTests.Data;
+
+namespace ObjectToTest.UnitTests.Models
+{
+    public class WithSingletonArgument
+    {
+        private readonly SingletonClass _singletonClass;
+
+        public WithSingletonArgument(SingletonClass singletonClass)
+        {
+            _singletonClass = singletonClass;
+        }
+    }
+}
