569a7b823ef7b4f8bc24e3dcd4460f3080fd419d
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-09-17 17:37:24 +0300

    Add puzzle for ObjectSharedArguments class

--- tests/ObjectToTest.UnitTests/Models/CircularRefPublicProperty2.cs
+++ tests/ObjectToTest.UnitTests/Models/CircularRefPublicProperty2.cs
@@ -2,6 +2,7 @@
 {
     public class CircularRefPublicProperty2
     {
-        public CircularRefPublicProperty1? PropertyName { get; set; }
+        public CircularRefPublicProperty1? PropertyName1 { get; set; }
+        public CircularRefPublicProperty3? PropertyName3 { get; set; }
     }
 }
