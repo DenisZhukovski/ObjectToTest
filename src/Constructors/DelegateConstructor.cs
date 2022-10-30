c8e882a299e0ea627e43ae403416009194a949a7
Denis Zhukovski <46710314+DenisZhukovski@users.noreply.github.com>
2022-10-30 16:00:04 +0300

    Il to csharp start (#55)
    * Add IL code reader
    
    * add unit tests
    
    * Add puzzle to generate c# code from IL
    
    * fix unit tests
    
    * fix unit tests
    
    * code refactoring
    
    * start working on IL to C# decompiler. As a staple package ILSpy.Decompiler has been chosen

--- src/Constructors/DelegateConstructor.cs
+++ src/Constructors/DelegateConstructor.cs
@@ -1,6 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.CSharp;
+using ICSharpCode.Decompiler.Metadata;
+using ICSharpCode.Decompiler.TypeSystem;
 using ObjectToTest.Arguments;
 
 namespace ObjectToTest.Constructors
@@ -30,9 +35,37 @@ namespace ObjectToTest.Constructors
             var names = string.Join(", ", parameters.Select(p => p.Name));
             if (parameters.Length == 1)
             {
-                return $"{names} => {{ }}";
+                return $"{names} => {DelegateBody()}";
             }
-            return $"({names}) => {{ }}";
+            return $"({names}) => {DelegateBody()}";
+        }
+
+        private string DelegateBody()
+        {
+            /*
+            * @todo #54:60m/LEAD
+             * Using new MethodBody(_object.Method) as a helper
+             * DelegateBody method should be able to generate the code base for the delegate to
+             * be able to pass the tests that reference DelegateConstructor class.
+             * See ObjectToTest.ILReader.MethodBody class which is able to generate
+             * IL code instructions based on byte array received from _object.Method.
+             * 
+             * TARGET:
+             * - NEED TO FIND a way to convert IL instructions into C# code.
+             * - To test the result DelegateConstructorTests skip marker should be removed.
+             * - Ideally all the tests should pass.
+            */
+            
+            var decompiler = new CSharpDecompiler(
+                _object.Target.GetType().Module.Assembly.Location,
+                new DecompilerSettings(LanguageVersion.Latest)
+            );
+            
+            var nameOfUniResolver =  new FullTypeName(_object.Target.GetType().FullName);
+            ITypeDefinition typeInfo = decompiler.TypeSystem.FindType(nameOfUniResolver).GetDefinition();
+            var tokenOfFirstMethod = typeInfo.Methods.First(m => m.Name == _object.Method.Name).MetadataToken;
+            var test = decompiler.DecompileAsString(tokenOfFirstMethod);
+            return "{ }";
         }
     }
 }
