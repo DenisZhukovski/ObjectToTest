fd7f4c34a194e51458ec6eeca611c6764cdd2a31
Denis Zhukovski <dzhukovsky@softek.by>
2022-11-28 14:41:30 +0300

    add new unit tests

---
+++ tests/ObjectToTest.UnitTests/ObjectDependenciesTraceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ObjectToTest.UnitTests.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ObjectToTest.UnitTests
+{
+    public class ObjectDependenciesTraceTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ObjectDependenciesTraceTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void IncorrectArgumentsClass()
+        {
+            Assert.Equal(
+                $"ctor IncorrectArgumentsClass{Environment.NewLine}" +
+                $"  int first - not found in object{Environment.NewLine}" +
+                $"  int second - not found in object{Environment.NewLine}",
+                new  ObjectDependenciesTrace(new IncorrectArgumentsClass(1, 2))
+                    .ToString()
+                    .Log(_output)
+            );
+        }
+    }
+}
