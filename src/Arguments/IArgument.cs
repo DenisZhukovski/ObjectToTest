49fcfdc8cb847773307147ed8012c7a57c9ec21f
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-11-29 17:03:00 +0300

    better comment

--- src/Arguments/IArgument.cs
+++ src/Arguments/IArgument.cs
@@ -7,7 +7,7 @@ namespace ObjectToTest.Arguments
         string Name { get;  }
 
         /// <summary>
-        /// It an argument type name
+        /// It an object type name which is encapsulated by argument 
         /// </summary>
         string Type { get; }
         
