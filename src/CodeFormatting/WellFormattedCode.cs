686e815814de33dc83b3529d16edeaa131406a74
stanislau_kanaplianik <stanislau_kanaplianik@epam.com>
2022-11-30 16:46:35 +0300

    WellFormattedCode with unit tests.
    (cherry picked from commit bf22d406dcf214a4b31c5c137e0aac9ee1000f88)

---
+++ src/CodeFormatting/WellFormattedCode.cs
@@ -0,0 +1,22 @@
+﻿namespace ObjectToTest.CodeFormatting
+{
+    /*
+     * @todo #85 60m/DEV Decompose code formatting to create syntax tree from code.
+     * Better to parse code and format it properly. It isolates existing functionality.
+     * Probably need to invest some time to find existing ability to create syntax tree from code.
+     */
+    public class WellFormattedCode
+    {
+        private readonly string _notFormattedCode;
+
+        public WellFormattedCode(string notFormattedCode)
+        {
+            _notFormattedCode = notFormattedCode;
+        }
+
+        public override string ToString()
+        {
+            return _notFormattedCode;
+        }
+    }
+}
