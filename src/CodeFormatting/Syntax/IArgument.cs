0ecb9f38df18a1361fa103bdb668dffcf0d3304e
Stanislau <Stanislau_Kanaplianik@epam.com>
2022-12-01 17:36:07 +0300

    Syntax tree. (#95)

---
+++ src/CodeFormatting/Syntax/IArgument.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectToTest.CodeFormatting.Syntax
+{
+    /// <summary>
+    /// new Foo([argument]).
+    /// foo.Run([argument]).
+    /// </summary>
+    public interface IArgument
+    {
+    }
+}
---
+++ src/CodeFormatting/Syntax/IInstantiationStatement.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectToTest.CodeFormatting.Syntax
+{
+    /// <summary>
+    /// new Foo(), new int[] {}, dictionary or list
+    /// </summary>
+    public interface IInstantiationStatement : IRightAssignmentPart, ICodeStatement, IArgument
+    {
+        /*
+         * @todo #91 60m/DEV Define necessary properties
+         * Most likely: new [TypeDefinition]([Arguments]) {[InlinePropertiesAssignment]}
+         */
+    }
+}
