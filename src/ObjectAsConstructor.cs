0fecc52b3fc17400db069b1e6ba21201a49b0b24
Denis Zhukovski <46710314+DenisZhukovski@users.noreply.github.com>
2022-12-02 14:30:15 +0300

    Documentation updates (#101)
    * code refactoring
    
    * documentation updates
    
    * update readme file
    
    * close the puzzles

--- src/ObjectAsConstructor.cs
+++ src/ObjectAsConstructor.cs
@@ -8,14 +8,6 @@ using ObjectToTest.Exceptions;
 
 namespace ObjectToTest
 {
-    /**
-    * @todo #60m/ARCH Need to create Interface/Class diagram that describes the solution on high level
-    * Ideally this diagram should be put into Docs folder of the repository and referenced in Readme file.
-    */
-    /**
-    * @todo #20 60m/ARCH Need to complete the diagram ObjectToTest/docs/ObjectToTest.drawio
-     * More details and text explanation should be added describing the main parsing flow.
-    */
     public class ObjectAsConstructor
     {
         private readonly object _object;
