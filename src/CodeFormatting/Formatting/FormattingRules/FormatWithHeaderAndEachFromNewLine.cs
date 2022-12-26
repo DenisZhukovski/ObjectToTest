4150bb7875362b9389fd677b8d5bf8a06e1340a9
Stanislau <Stanislau_Kanaplianik@epam.com>
2022-12-26 13:01:31 +0300

    DumpHierarchy draft implementation. (#169)
    * DumpHierarchy draft implementation.
    
    * Fix naming.

---
+++ src/CodeFormatting/Formatting/FormattingRules/FormatWithHeaderAndEachFromNewLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ObjectToTest.Extensions;
+
+namespace ObjectToTest.CodeFormatting.Formatting
+{
+    public class FormatWithHeaderAndEachFromNewLine
+    {
+        private readonly Func<string[], string> _header;
+        private readonly string[] _args;
+        private readonly Tabs _tabs;
+
+        public FormatWithHeaderAndEachFromNewLine(Func<string[], string> header, string[] args, Tabs tabs)
+        {
+            _header = header;
+            _args = args;
+            _tabs = tabs;
+        }
+
+        public (string, Tabs) Format()
+        {
+            var format = new Parts(
+                _tabs.Tab() + _header(_args) + Environment.NewLine,
+                _args.FormatEach(argument => $"{_tabs.Tab().Tab()}- Argument: {argument.String}{Environment.NewLine.If(argument.IsNotLast)}").ToString()
+            );
+
+            return (format.ToString(), _tabs.Tab().Tab());
+        }
+    }
+}
---
+++ src/CodeFormatting/Syntax/Extensions/SyntaxTreeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ObjectToTest.CodeFormatting.Formatting;
+using ObjectToTest.CodeFormatting.Syntax.Contracts;
+
+namespace ObjectToTest.CodeFormatting.Syntax.Extensions
+{
+    public static class SyntaxTreeExtensions
+    {
+        public static string Dump(this ISyntaxTree syntaxTree)
+        {
+            return new SyntaxTreeDump(syntaxTree).ToString();
+        }
+    }
+}
