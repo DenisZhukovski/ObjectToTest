729c863583121157b926ad2dd429b3f589d5b79a
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2022-12-29 13:31:59 +0300

    anonymous array type definition

--- src/CodeFormatting/Syntax/Statements/Instantiation/RawTypeDefinition.cs
+++ src/CodeFormatting/Syntax/Statements/Instantiation/RawTypeDefinition.cs
@@ -1,19 +1,30 @@
 ﻿using ObjectToTest.CodeFormatting.Syntax.Contracts;
+using ObjectToTest.CodeFormatting.Syntax.Core.Strings;
 
 namespace ObjectToTest.CodeFormatting.Syntax.Statements.Instantiation
 {
     public class RawTypeDefinition : ITypeDefinition
     {
-        private readonly string _code;
+        private readonly ISubstring _typeAsString;
 
         public RawTypeDefinition(string code)
+            : this(
+                new FirstOf(
+                    new SubstringBetween(code, "new ", "("),
+                    new SubstringBetween(code, "new", "{")
+                )
+            )
         {
-            _code = code;
+        }
+
+        public RawTypeDefinition(ISubstring typeAsString)
+        {
+            _typeAsString = typeAsString;
         }
 
         public override string ToString()
         {
-            return _code;
+            return _typeAsString.ToString().Trim();
         }
     }
 }
